9d3e1ed6de26b3dc780a72b42bc201640d7ae88d
Minh Luan Tran <noblakit01@gmail.com>
2017-07-25 01:03:03 -0700

    :ambulance: Add Demo project

---
+++ SwiftyImageCacheDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyImageCacheDemo
+//
+//  Created by Minh Luan Tran on 7/25/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
