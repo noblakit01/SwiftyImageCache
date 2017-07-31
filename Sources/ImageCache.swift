fb2eb480467bca802efb87124f49296779f7f680
noblakit01 <noblakit01@gmail.com>
2017-07-31 22:54:40 +0700

    Get image from disk cache

--- Sources/ImageCache.swift
+++ Sources/ImageCache.swift
@@ -22,13 +22,15 @@ open class ImageCache {
     
     open func loadImage(atUrl url: URL, completion: ((String, UIImage?) -> Void)? = nil) {
         let urlString = url.absoluteString
-        if let image = images.object(forKey: urlString as NSString) {
+        let key = urlString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? urlString
+        if let image = image(of: key) {
             completion?(urlString, image)
             return
         }
-        if let workItem = workItems.object(forKey: urlString as NSString) {
+        
+        if let workItem = workItems.object(forKey: key as NSString) {
             workItem.notify(queue: queue, execute: { [weak self] in
-                if let image = self?.images.object(forKey: urlString as NSString) {
+                if let image = self?.images.object(forKey: key as NSString) {
                     DispatchQueue.main.async {
                         completion?(urlString, image)
                     }
@@ -39,16 +41,7 @@ open class ImageCache {
         let workItem = DispatchWorkItem { [weak self] in
             do {
                 let data = try Data(contentsOf: url)
-                if let image = UIImage(data: data) {
-                    if let fileName = urlString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
-                        let fileURL = cacheFileUrl(fileName)
-                        do {
-                            try data.write(to: fileURL, options: Data.WritingOptions.atomic)
-                        } catch (let error) {
-                            print("Error write file \(error.localizedDescription)")
-                        }
-                    }
-                    self?.images.setObject(image, forKey: urlString as NSString)
+                if let image = self?.cacheImage(data: data, key: key) ?? UIImage(data: data) {
                     DispatchQueue.main.async {
                         completion?(urlString, image)
                     }
@@ -61,7 +54,7 @@ open class ImageCache {
                 completion?(urlString, nil)
             }
         }
-        workItems.setObject(workItem, forKey: urlString as NSString)
+        workItems.setObject(workItem, forKey: key as NSString)
         queue.async(execute: workItem)
     }
     
@@ -70,5 +63,35 @@ open class ImageCache {
         images.removeAllObjects()
     }
     
+    func image(of key: String) -> UIImage? {
+        if let image = images.object(forKey: key as NSString) {
+            return image
+        }
+        let fileURL = cacheFileUrl(key)
+        do {
+            let data = try Data(contentsOf: fileURL)
+            return UIImage(data: data)
+        } catch (let error) {
+            print("Can read with error \(error.localizedDescription)")
+        }
+        return nil
+    }
     
+    func cacheImage(data: Data, key: String) -> UIImage? {
+        switch cacheType {
+        case .ram:
+            if let image = UIImage(data: data) {
+                images.setObject(image, forKey: key as NSString)
+                return image
+            }
+        case .disk:
+            let fileURL = cacheFileUrl(key)
+            do {
+                try data.write(to: fileURL, options: Data.WritingOptions.atomic)
+            } catch (let error) {
+                print("Error write file \(error.localizedDescription)")
+            }
+        }
+        return nil
+    }
 }
