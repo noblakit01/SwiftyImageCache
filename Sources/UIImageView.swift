8ea22284ba531fc42823d2259024f6ebe2f7101b
Minh Luan Tran <noblakit01@gmail.com>
2017-08-04 10:51:10 +0700

    Add cache and completion as parameter

--- Sources/UIImageView.swift
+++ Sources/UIImageView.swift
@@ -9,10 +9,11 @@
 import UIKit
 
 extension UIImageView {
-    public func setUrl(_ url: URL, qualityFactor: CGFloat? = nil) {
+    public func setUrl(_ url: URL, qualityFactor: CGFloat? = nil, cache: ImageCache = .default, completion: ((UIImage?) -> Void)? = nil) {
         let fitSize = qualityFactor != nil ? bounds.size * qualityFactor! : nil
-        ImageCache.default.loadImage(atUrl: url, fitSize: fitSize) { [weak self] (url, image) in
+        cache.loadImage(atUrl: url, fitSize: fitSize) { [weak self] (url, image) in
             self?.image = image
+            completion?(image)
         }
     }
 }
