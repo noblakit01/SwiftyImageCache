cee2d39a372afad6a1d2bd56e8bd660bcdb0ed6a
noblakit01 <noblakit01@gmail.com>
2017-08-07 22:44:03 +0700

    Create Swift Url Key

--- Sources/UIImageView.swift
+++ Sources/UIImageView.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
+let _swiftUrlKey = malloc(4)
+
 extension UIImageView {
     public func setUrl(_ url: URL, qualityFactor: CGFloat? = nil, cache: ImageCache = .default, completion: ((UIImage?) -> Void)? = nil) {
         let fitSize = qualityFactor != nil ? bounds.size * qualityFactor! : nil
         cache.loadImage(atUrl: url, fitSize: fitSize) { [weak self] (url, image) in
             self?.image = image
             completion?(image)
+        }
+    }
+    
+    fileprivate func value<T>(_ key:UnsafeMutableRawPointer?, _ defaultValue:T) -> T {
+        return (objc_getAssociatedObject(self, key) as? T) ?? defaultValue
+    }
+    
+    private var swiftUrlKey: String {
+        get {
+            return value(_swiftUrlKey, "")
+        }
+        set {
+            objc_setAssociatedObject(self, _swiftUrlKey, newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN);
         }
     }
 }
